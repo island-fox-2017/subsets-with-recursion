bdaffc4a8649d62a22cb9058af8712c29e4f4b2a
Ahmad Fajar <shin.fajar@gmail.com>
2017-06-21 20:11:54 +0700

    ahmad fajar - subset with recursion

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,7 +1,12 @@
+'use strict'
+
 let choose_team = (n, k) => {
-  //write your code here
+  if (k === 1) return n;
+  if (n === k) return 1;
+
+  return choose_team(n - 1, k - 1) + choose_team(n - 1, k);
 }
 
-console.log(choose_team(6, 2));
-console.log(choose_team(6, 3));
-console.log(choose_team(24, 4));
+console.log(choose_team(6, 2));  // 15
+console.log(choose_team(6, 3));  // 20
+console.log(choose_team(24, 4)); // 10626

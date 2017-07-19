91e12196dd6724dedb2d736460e623f3b67fd9ac
adnin31 <adnin.ris31@gmail.com>
2017-07-19 11:09:04 +0700

    baru selsesi sekarang

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,7 +1,16 @@
 let choose_team = (n, k) => {
   //write your code here
+  if( k==1){
+    return  n;
+  }
+  else if (n ==1) {
+    return 1
+  }
+  console.log("ini ----->",n);
+  console.log("ini -----> k",k);
+  return choose_team(n-1,k-1) + choose_team(n-1,k)
 }
 
 console.log(choose_team(6, 2));
-console.log(choose_team(6, 3));
-console.log(choose_team(24, 4));
+// console.log(choose_team(6, 3));
+// console.log(choose_team(24, 4));

cf61771845bd2051c5a3250ffb009384dd11cd46
kokoh <kokohtan@gmail.com>
2017-07-20 08:54:19 +0700

    subsetrecursion

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,7 +1,10 @@
 let choose_team = (n, k) => {
   //write your code here
+  if(k === 1){ return n }
+  if(n === k){ return 1 }
+  return  choose_team(n-1, k-1) + choose_team(n-1,k)
 }
 
-console.log(choose_team(6, 2));
-console.log(choose_team(6, 3));
-console.log(choose_team(24, 4));
+console.log('aaa', choose_team(6, 2));
+console.log('bbb', choose_team(6, 3));
+console.log('ccc',choose_team(24, 4));

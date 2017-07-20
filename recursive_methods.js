9cb7f8a3441de66455b918d685a5adf7ab5fe9c4
achimbaggins <achim_baggins@yahoo.com>
2017-07-20 14:57:01 +0700

    update condition

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,6 +1,8 @@
 let choose_team = (n, k) => {
   //write your code here
-  if (k === 1){
+  if (n < k) {
+    return -1;
+  } else if (k === 1){
     return n;
   } else if(n === k){
     return 1;
@@ -10,6 +12,6 @@ let choose_team = (n, k) => {
 
 }
 
-console.log(choose_team(6, 2));
+console.log(choose_team(1, 2));
 console.log(choose_team(6, 3));
 console.log(choose_team(24, 4));
